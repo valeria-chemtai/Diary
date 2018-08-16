811e71da2e4cc0b277738cd7dca818d36aaf6563
valeria-chemtai <valeriachemtai28@gmail.com>
2018-08-16 16:12:58 +0300

    [feat] add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, blank=True, null=True,
+                                on_delete=models.CASCADE)
+    bio = models.TextField(max_length=250, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=60)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    user_id = models.ForeignKey(Profile, related_name='entries',
+                                on_delete=models.CASCADE)
+
+
+class Content(models.Model):
+    text = models.TextField(max_length=500)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    entry_id = models.ForeignKey(Entry, related_name='contents',
+                                 on_delete=models.CASCADE)
